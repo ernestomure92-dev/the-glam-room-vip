2aaa473f4137bcbbee5c9c366b6ebf820056d64c
ernestomure92-dev <ernestomure92@gmail.com>
2026-03-01 23:33:24 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,20 @@
-// REEMPLAZA CON TUS DATOS DE FIREBASE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "the-glam-room.firebaseapp.com",
-    databaseURL: "https://the-glam-room-default-rtdb.firebaseio.com",
-    projectId: "the-glam-room",
-    storageBucket: "the-glam-room.appspot.com",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+  apiKey: "AIzaSyBKBrQgkoJ1IDwcdkrAdfCjWANm9J6bNkc",
+  authDomain: "theglamroom-mx.firebaseapp.com",
+  projectId: "theglamroom-mx",
+  storageBucket: "theglamroom-mx.firebasestorage.app",
+  messagingSenderId: "57431429937",
+  appId: "1:57431429937:web:4139ca449dfac178a05610"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
