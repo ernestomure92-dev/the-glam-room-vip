b9195894d53e19d1ce4f795488dcdcbb8da3ff7e
ernestomure92-dev <ernestomure92@gmail.com>
2026-03-02 11:59:50 -0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// ==========================================
+// CONFIGURACIÓN FIREBASE - THE GLAM ROOM
+// ==========================================
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBKBrQgkoJ1IDwcdkrAdfCjWANm9J6bNkc",
-  authDomain: "theglamroom-mx.firebaseapp.com",
-  projectId: "theglamroom-mx",
-  storageBucket: "theglamroom-mx.firebasestorage.app",
-  messagingSenderId: "57431429937",
-  appId: "1:57431429937:web:4139ca449dfac178a05610"
+    apiKey: "AIzaSyBKBrOgkoJ1IDwcDkrAdFCjWAnm9J6bNKc",
+    authDomain: "theglamroom-mx.firebaseapp.com",
+    databaseURL: "https://theglamroom-mx-default-rtdb.firebaseio.com",
+    projectId: "theglamroom-mx",
+    storageBucket: "theglamroom-mx.firebasestorage.app",
+    messagingSenderId: "57431429937",
+    appId: "1:57431429937:web:4139ca4439dfac178a05610"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
+// Inicializar Firebase (versión compat)
 firebase.initializeApp(firebaseConfig);
+
+// Referencias a la base de datos
 const database = firebase.database();
 const clientsRef = database.ref('clients');
 const visitsRef = database.ref('visits');
